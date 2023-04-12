528aba24e29132c8f0c01803f633bd2407d0163c
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-04-12 11:16:11 -0300

    Implementando listagem de pacientes

--- src/main/java/med/voll/apiMed/controller/PacienteController.java
+++ src/main/java/med/voll/apiMed/controller/PacienteController.java
@@ -1,8 +1,12 @@
 package med.voll.apiMed.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import jakarta.validation.Valid;
 import med.voll.apiMed.paciente.DadosCadastroPaciente;
 import med.voll.apiMed.paciente.DadosDetalhamentoPaciente;
+import med.voll.apiMed.paciente.DadosListagemPaciente;
 import med.voll.apiMed.paciente.Paciente;
 import med.voll.apiMed.paciente.PacienteRepository;
 
@@ -31,6 +36,13 @@ public class PacienteController {
 		var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
 		
 		return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
+	}
+	
+	@GetMapping
+	public ResponseEntity <Page<DadosListagemPaciente>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+		var page = repository.findAll(paginacao).map(DadosListagemPaciente::new);
+		
+		return ResponseEntity.ok(page);
 	}
 
 }
