997cce376da54ac44a728a451b0dca862f2ba28c
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-04-13 16:05:30 -0300

    Tratando erro 400, para argumentos inválidos

--- src/main/java/med/voll/apiMed/infra/TratadorDeErros.java
+++ src/main/java/med/voll/apiMed/infra/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package med.voll.apiMed.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,24 @@ public class TratadorDeErros { // classe que isola o tratamento de exceções da
 	@ExceptionHandler(EntityNotFoundException.class) // onde for lançada esta exception
 	public ResponseEntity tratarErro404() {
 		return ResponseEntity.notFound().build();
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class) // argumento não valido, faltando
+	public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors(); // retorna lista
+		
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList()); // faz conversão de uma lista para outra
+	}
+	
+	
+	
+	
+	private record DadosErroValidacao(String campo, String mensagem) { // DTO dentro da classe, só vai ser usada aqui
+		
+		public DadosErroValidacao(FieldError erro){
+			this(erro.getField(), erro.getDefaultMessage()); // field da o nome do campo, e default da a mensagem pra aquele campo especifico
+		}
+		
 	}
 	
 }
