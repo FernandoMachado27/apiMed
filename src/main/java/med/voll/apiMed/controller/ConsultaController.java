ae7f41c7ac428ef98c37b192ba9975ae8e52f38b
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-04-18 15:57:58 -0300

    Adicionando metodo cancelamento de consulta

--- src/main/java/med/voll/apiMed/controller/ConsultaController.java
+++ src/main/java/med/voll/apiMed/controller/ConsultaController.java
@@ -3,6 +3,8 @@ package med.voll.apiMed.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.apiMed.domain.consulta.AgendaDeConsultas;
+import med.voll.apiMed.domain.consulta.ConsultaRepository;
 
 @RestController
 @RequestMapping("/consultas")
@@ -18,6 +21,9 @@ public class ConsultaController {
 	@Autowired
 	private AgendaDeConsultas agenda;
 	
+	@Autowired
+	private ConsultaRepository repository;
+	
 	@PostMapping
 	@Transactional
 	public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
@@ -25,6 +31,14 @@ public class ConsultaController {
 		agenda.agendar(dados);
 		
 		return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity cancelarConsulta(@PathVariable Long id) {
+		repository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
