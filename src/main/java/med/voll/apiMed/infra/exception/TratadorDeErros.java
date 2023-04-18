903c6d693a7089cb06f4035ce798e260a06a1cb9
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-04-17 22:45:37 -0300

    Implementando mais alguns erros comuns a classe TratadorDeErros

--- src/main/java/med/voll/apiMed/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/apiMed/infra/exception/TratadorDeErros.java
@@ -1,6 +1,12 @@
 package med.voll.apiMed.infra.exception;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,8 +29,30 @@ public class TratadorDeErros { // classe que isola o tratamento de exceções da
 		return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList()); // faz conversão de uma lista para outra
 	}
 	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
 	
-	
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity tratarErroBadCredentials() {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+	}
+
+	@ExceptionHandler(AuthenticationException.class)
+	public ResponseEntity tratarErroAuthentication() {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity tratarErroAcessoNegado() {
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity tratarErro500(Exception ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + ex.getLocalizedMessage());
+	}
 	
 	private record DadosErroValidacao(String campo, String mensagem) { // DTO dentro da classe, só vai ser usada aqui
 		
