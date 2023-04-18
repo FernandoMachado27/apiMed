f10965bbc9e5e86cd0eac0a823b0d204a02d0784
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-04-18 12:08:38 -0300

    Validações de integridade da classe AgendaDeConsultas

---
+++ src/main/java/med/voll/apiMed/domain/ValidacaoException.java
@@ -0,0 +1,8 @@
+package med.voll.apiMed.domain;
+
+public class ValidacaoException extends RuntimeException {
+	
+	public ValidacaoException(String mensagem) {
+		super(mensagem);
+	}
+}
--- src/main/java/med/voll/apiMed/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/apiMed/domain/consulta/AgendaDeConsultas.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import med.voll.apiMed.controller.DadosAgendamentoConsulta;
+import med.voll.apiMed.domain.ValidacaoException;
+import med.voll.apiMed.domain.medico.Medico;
 import med.voll.apiMed.domain.medico.MedicoRepository;
 import med.voll.apiMed.domain.paciente.PacienteRepository;
 
@@ -20,11 +22,23 @@ public class AgendaDeConsultas {
 	private PacienteRepository pacienteRepository;
 	
 	public void agendar(DadosAgendamentoConsulta dados) {
+		if (!pacienteRepository.existsById(dados.idPaciente())) {
+			throw new ValidacaoException("Id do paciente não existe!");
+		}
 		
-		var medico = medicoRepository.findById(dados.idMedico()).get(); // .get() para pegar o obj e não o optional
-		var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+		if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) { // medico opcional
+			throw new ValidacaoException("Id do médico não existe!");
+		}
+		
+		var paciente = pacienteRepository.findById(dados.idPaciente()).get(); // .get() para pegar o obj e não o optional
+		var medico = escolherMedico(dados);
 		var consulta = new Consulta(null, medico, paciente, dados.data());
 		consultaRepository.save(consulta);
+	}
+
+	private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+		return null;
+		
 	}
 
 }
